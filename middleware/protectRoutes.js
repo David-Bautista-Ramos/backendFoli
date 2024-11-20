b20e9bba8d92bb57eff141be2dd80ff186bffedc
DavidBautista <davidalejandrobautistaramos@gmail.com>
2024-11-20 08:38:07 -0500

    prubaAaa token otra vez ahhhhhh dios

--- middleware/protectRoutes.js
+++ middleware/protectRoutes.js
@@ -10,22 +10,22 @@ export const protectRoutes = async (req, res, next)=>{
         if(!token) {
             return res.status(401).json({error: "Sin Autorización: No proporiona el Token"});
         }
-        jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
-            if (err) {
-                console.log("Token inválido:", err.message); // Log de error para diagnosticar problemas
-                return res.status(401).json({ error: "Sin Autorización: Token inválido" });
-            }
-            console.log("Token decodificado:", decoded); // Log para verificar el contenido del token
-            // Buscar al usuario en la base de datos
-            const user = await User.findById(decoded.userId).select("-contrasena");
-            if (!user) {
-                console.log("Usuario no encontrado");
-                return res.status(404).json({ error: "Usuario no encontrado" });
-            }
-            // Asignar el usuario a req.user
-            req.user = user;
-            next(); // Continuar con la siguiente función
-        });
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log('Token decodificado:', decoded); // Log para verificar el contenido del token
+        if (!decoded) {
+            console.log('Sin autorización: Token inválido');
+            return res.status(401).json({ error: "Sin Autorización: Token inválido" });
+        }
+        // Buscar al usuario en la base de datos
+        const user = await User.findById(decoded.userId).select("-contrasena");
+        console.log('Usuario encontrado:', user); // Log para confirmar que el usuario existe
+        if (!user) {
+            console.log('Usuario no encontrado');
+            return res.status(404).json({ error: "Usuario no encontrado" });
+        }
+        // Asignar el usuario a req.user para las siguientes rutas
+        req.user = user;
+        next();
     } catch (error) {
         console.log("Error in protectRoutes middleware",error.message);
         return res.status(500).json({error:"Error server interno"})
