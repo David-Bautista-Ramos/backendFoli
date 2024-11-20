21e2a7982a3bbcb1fbf9630948237d4fa1684c0a
DavidBautista <davidalejandrobautistaramos@gmail.com>
2024-11-20 10:28:02 -0500

    prubaAaa token otra vez ahhhhhh dios jajajajkk

--- middleware/protectRoutes.js
+++ middleware/protectRoutes.js
@@ -1,35 +1,31 @@
-import User from '../models/user.model.js';
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import User from '../models/user.model.js';
 
-dotenv.config(); // Asegúrate de cargar las variables de entorno
+dotenv.config();
 
-export const protectRoutes = async (req, res, next)=>{
-    try {
-        const token = req.headers['authorization']?.split(' ')[1];
-        console.log('Token recibido:', token); // Log para verificar si el token se recibe
-
-        if(!token) {
-            return res.status(401).json({error: "Sin Autorización: No proporiona el Token"});
-        }
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        console.log('Token decodificado:', decoded); // Log para verificar el contenido del token
-        if (!decoded) {
-            console.log('Sin autorización: Token inválido');
-            return res.status(401).json({ error: "Sin Autorización: Token inválido" });
-        }
-        // Buscar al usuario en la base de datos
-        const user = await User.findById(decoded.userId).select("-contrasena");
-        console.log('Usuario encontrado:', user); // Log para confirmar que el usuario existe
-        if (!user) {
-            console.log('Usuario no encontrado');
-            return res.status(404).json({ error: "Usuario no encontrado" });
-        }
-        // Asignar el usuario a req.user para las siguientes rutas
-        req.user = user;
-        next();
-    } catch (error) {
-        console.log("Error in protectRoutes middleware",error.message);
-        return res.status(500).json({error:"Error server interno"})
+export const protectRoutes = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(' ')[1]; // 'Bearer <token>'
+    
+    if (!token) {
+      return res.status(401).json({ error: "No se proporcionó el token" });
     }
-}
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET); // Verificar el token con la clave secreta
+    if (!decoded) {
+      return res.status(401).json({ error: "Token inválido" });
+    }
+
+    const user = await User.findById(decoded.userId).select("-password"); // Obtener el usuario por el ID en el token
+    if (!user) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
+    }
+
+    req.user = user; // Asignar usuario al objeto de la solicitud para usar en el resto de las rutas
+    next(); // Continuar con la siguiente función del middleware
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Error en el servidor" });
+  }
+};
