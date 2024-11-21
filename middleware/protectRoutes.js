c48435bc282feed0ebddd1da3d18fe7cce5a1762
DavidBautista <davidalejandrobautistaramos@gmail.com>
2024-11-20 21:25:39 -0500

    prubaAaa token otra vez ahhhhhh dios jajajajkkk

--- middleware/protectRoutes.js
+++ middleware/protectRoutes.js
@@ -6,24 +6,28 @@ dotenv.config();
 
 export const protectRoutes = async (req, res, next) => {
   try {
-    const token = req.headers.authorization?.split(' ')[1]; // 'Bearer <token>'
+    const token = req.headers['authorization']?.split(' ')[1];
     console.log("el token:" , token)
     if (!token) {
       return res.status(401).json({ error: "No se proporcionó el token" });
     }
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET); // Verificar el token con la clave secreta
-    if (!decoded) {
-      return res.status(401).json({ error: "Token inválido" });
-    }
-
-    const user = await User.findById(decoded.userId).select("-password"); // Obtener el usuario por el ID en el token
-    if (!user) {
-      return res.status(404).json({ error: "Usuario no encontrado" });
-    }
-
-    req.user = user; // Asignar usuario al objeto de la solicitud para usar en el resto de las rutas
-    next(); // Continuar con la siguiente función del middleware
+    
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+        if (err) {
+            console.log("Token inválido:", err.message); // Log de error para diagnosticar problemas
+            return res.status(401).json({ error: "Sin Autorización: Token inválido" });
+        }
+        console.log("Token decodificado:", decoded); // Log para verificar el contenido del token
+        // Buscar al usuario en la base de datos
+        const user = await User.findById(decoded.userId).select("-contrasena");
+        if (!user) {
+            console.log("Usuario no encontrado");
+            return res.status(404).json({ error: "Usuario no encontrado" });
+        }
+        // Asignar el usuario a req.user
+        req.user = user;
+        next(); // Continuar con la siguiente función
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: "Error en el servidor" });
